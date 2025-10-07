4b60a26c2bf68a41ef3338d69e9beac87b18cb3e
RichardGL11 <100035136+RichardGL11@users.noreply.github.com>
2025-10-07 00:34:14 +0000

    Fix styling

--- src/Core/Listeners/External/HardBouncedMailListener.php
+++ src/Core/Listeners/External/HardBouncedMailListener.php
@@ -4,7 +4,6 @@ namespace Basement\BetterMails\Core\Listeners\External;
 
 use Basement\BetterMails\Core\Concerns\HasMail;
 use Basement\BetterMails\Core\Contracts\External\HardBouncedEventContract;
-use Basement\BetterMails\Core\Models\BetterEmail;
 
 final class HardBouncedMailListener
 {
