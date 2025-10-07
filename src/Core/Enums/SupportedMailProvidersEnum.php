a88834facdbd70617cab48d7a1b4a52f9a29a01c
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-10-07 17:43:08 -0300

    feat(Middleware): resend middleware through enums

--- src/Core/Enums/SupportedMailProvidersEnum.php
+++ src/Core/Enums/SupportedMailProvidersEnum.php
@@ -2,7 +2,17 @@
 
 namespace Basement\BetterMails\Core\Enums;
 
+use Basement\BetterMails\Resend\Email\Middleware\VerifyResendWebhookSignature;
+use Resend\Laravel\Http\Middleware\VerifyWebhookSignature;
+
 enum SupportedMailProvidersEnum: string
 {
     case Resend = 'resend';
+
+    public function getMiddleware(): array
+    {
+        return match ($this) {
+            self::Resend => [VerifyWebhookSignature::class, VerifyResendWebhookSignature::class],
+        };
+    }
 }
--- src/Core/Http/Controllers/WebhookController.php
+++ src/Core/Http/Controllers/WebhookController.php
@@ -3,12 +3,18 @@
 namespace Basement\BetterMails\Core\Http\Controllers;
 
 use Basement\BetterMails\Core\Contracts\BetterDriverContract;
+use Basement\BetterMails\Core\Enums\SupportedMailProvidersEnum;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
-final class WebhookController
+final class WebhookController extends Controller
 {
-    public function __invoke(Request $request, BetterDriverContract $driver)
+    public function __invoke(Request $request, BetterDriverContract $driver, string $provider)
     {
+        $provider = SupportedMailProvidersEnum::tryFrom($provider);
+        $this->middleware(
+            $provider->getMiddleware(),
+        );
         $driver->handle($request->all());
     }
 }
